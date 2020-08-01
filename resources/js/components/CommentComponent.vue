0c2110d0e2588b742534eaf14c0af1b4048444ce
rjmchi <robert@rjmprogramming.com>
2020-08-01 16:56:52 -0500

    Adds Comment component

---
+++ resources/js/components/CommentComponent.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+      <h1>Comments</h1>
+      <table>
+          <tr>
+              <th>Comment</th>
+              <th>Initials</th>
+          </tr>
+          <tr v-for="(comment, idx) in comments" v-bind:key='idx'>
+              <td>{{comment.comment}}</td>
+              <td>{{comment.initials}}</td>
+          </tr>
+          </table>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+        comments:[],
+    }
+  },
+  methods: {
+      fetchComments() {
+          fetch('api/comments')
+          .then(res=>res.json())
+          .then(res=>{
+              this.comments = res
+              console.log(res);
+          });
+      }
+  },
+  created() {
+      this.fetchComments();
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+    h1 {color:red;}
+</style>
